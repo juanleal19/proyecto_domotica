630fb745c8e693485dd567297d35fa89b7f5b702
Juan Leal <jsleal19@gmail.com>
2021-10-05 19:01:44 -0500

    ya funciona la primera parte de la ventana DX

--- hdi.py
+++ hdi.py
@@ -4,11 +4,17 @@ import tkinter
 window = Tk()
 window.geometry('600x500')
 
+def temperatura ():
+    pass
+
 if __name__ == '__main__':
     
     etiqueta = tkinter.Label(window, text='Sistema domotico')
     etiqueta.pack()
     
+    bototon1 = tkinter.Button(window, text = 'temperatura', command= temperatura)
+    bototon1.pack(side= tkinter.RIGHT)
+
     window.title("SIS domotico")
 
     window.mainloop()
