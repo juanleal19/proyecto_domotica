f5a98de8cc7a4f0e095d84b6d1826c1e0d562076
Juan Leal <jsleal19@gmail.com>
2021-10-05 20:07:23 -0500

    ya lee los valores del arduino

--- hdi.py
+++ hdi.py
@@ -1,11 +1,32 @@
 from tkinter import *
 import tkinter
-  
-window = Tk()
-window.geometry('600x500')
+import serial 
+import time
 
+
+
+window = Tk()
+window.geometry('600x500') #tamano de la ventana
+
+serialArduino = serial.Serial("COM6", 9600)
+time.sleep(1)
+
+
+#funcion que se ejecurta al optrimir el boton temperatura
 def temperatura ():
+    while True:
+        serialArduino.write(b'a')
+        val = serialArduino.readline().decode('ascii')
+        print(val)
+        print("*****************")
+
+        if val == 'e':
+            pass
+       
+def exit():
     pass
+
+
 
 if __name__ == '__main__':
     
