c134a66939e888c7b569adb4f9379f4f3335eb86
juan sebastian leal <jsleal19@gmail.com>
2021-11-09 18:15:23 -0500

    temperatura ready

--- hdi.py
+++ hdi.py
@@ -2,7 +2,6 @@ from tkinter import *
 import tkinter
 import serial 
 import time
-import matplotlib.pyplot as plt
 
 
 
@@ -17,25 +16,15 @@ time.sleep(1)
 def temperatura ():
 
     datos_tem =[]
-
-    #while True:
     for i in range(0,10):
-        serialArduino.write(b'a')
+        serialArduino.write(b'2')
         val = serialArduino.readline().decode('ascii')
         print(val)
-        slc = slice(27,32,1)
-        datos_tem.append(val[slc])
+        datos_tem.append(val)
         print("*****************")
     
     print(datos_tem)
-    serialArduino.write(b'e')
-    val = serialArduino.readline().decode('ascii')
-    print(val)
-
-    x = [1, 2,3 ,4 ,5, 6, 7, 8, 9,10]
-    # hacer la grafica de los datos de temperatura
-  #  plt.plot(x, datos_tem)
-
+    
 
 def exit():
     serialArduino.write(b'e')
