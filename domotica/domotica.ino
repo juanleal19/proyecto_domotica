cdaee6145cf97cb611756db67aa5716efa8f4673
juan sebastian leal <jsleal19@gmail.com>
2021-11-09 17:58:58 -0500

    nuevo codigo arduino

--- domotica/domotica.ino
+++ domotica/domotica.ino
@@ -1,10 +1,48 @@
-// codigo usado en el arduino
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+ 
+}
+ 
+int humedad()
+{
+  delay(500);
+  float h = dht.readHumidity();
+  if (isnan(h) ) {
+    Serial.println("Error obteniendo los datos del sensor DHT22");
+    return;
+  } 
+  return (h) ;
+}
+int temperatura()
+{
+  delay(500);  
+  float t = dht.readTemperature();
+ 
+  if ( isnan(t)) {
+    Serial.println("Error obteniendo los datos del sensor DHT22");
+    return;
+  } 
+  return (t) ;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+char Dato = Serial.read();
 
+  if(Dato == '1')
+  {
+    Serial.println(humedad());
+  }
+  else if (Dato == '2')
+  {
+    Serial.println(temperatura());
+  }
+  
+  
 }
