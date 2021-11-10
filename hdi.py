7c00ba2ca30f3e4bb5c2d75754be41870d218e61
juan sebastian leal <jsleal19@gmail.com>
2021-11-10 01:18:53 -0500

    funciona la grafica de temperatura y nada mas ese codigo esta asqueroso

--- hdi.py
+++ hdi.py
@@ -15,51 +15,70 @@ serialArduino = serial.Serial("COM4", 9600)
 time.sleep(1)
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-xdat , tdat ,hdat= [],[],[]
+xdat,xdat2,tdat ,hdat= [],[],[],[]
 
 
 #funcion que se ejecurta al optrimir el boton temperatura
 def temperatura ():
-    for i in range(0,10):
-        serialArduino.write(b'2')
-        val = serialArduino.readline().decode('ascii')
-        val2 = slice(0,2,1)
-        datos_tem = (val[val2])
+    serialArduino.write(b'2')
+    val = serialArduino.readline().decode('ascii')
+    val2 = slice(0,2,1)
+    datos_tem = (val[val2])
     return datos_tem
 
+def temperaturaPrnt ():
+    serialArduino.write(b'2')
+    val = serialArduino.readline().decode('ascii')
+    val2 = slice(0,2,1)
+    datos_tem = (val[val2])
+    print(datos_tem)
+
 def humedad ():
-    for i in range(0,10):
-        serialArduino.write(b'2')
-        val = serialArduino.readline().decode('ascii')
-        val2 = slice(0,2,1)
-        datos_hum= (val[val2])
+    serialArduino.write(b'1')
+    val = serialArduino.readline().decode('ascii')
+    val2 = slice(0,2,1)
+    datos_hum= (val[val2])
     return datos_hum
+
+def humedadPrnt():
+    serialArduino.write(b'1')
+    val = serialArduino.readline().decode('ascii')
+    val2 = slice(0,2,1)
+    datos_hum= (val[val2])
+    print( datos_hum)
 
 
 def exit():
-    serialArduino.write(b'e')
-
-
-def animate(i,xdat,tdat):
-    data = temperatura()
-    xdat.append(i)
-    tdat.append(data)
     ax.clear()
-    ax.plot(xdat, tdat)
     
-def animate2(i,xdat,hdat):
-    data = humedad()
+
+
+def animate(i,xdat,tdat, hdat):
+    data = temperatura()
+    data2 = humedad()
+    
     xdat.append(i)
     tdat.append(data)
+    hdat.append(data2)
+    
     ax.clear()
-    ax.plot(xdat, hdat)
+    ax.plot(xdat, tdat,hdat)
+    
+def animate2(j,xdat2,hdat):
+    data2 = humedad()
+    xdat2.append(j)
+    tdat.append(data2)
+    ax.clear()
+    ax.plot(xdat2, hdat)
 
 def grafica():
+    i = 0
     ani = animation.FuncAnimation(fig, animate,fargs=(xdat, tdat))
     plt.show()
 
 def grafica2():
-    ani = animation.FuncAnimation(fig, animate2,fargs=(xdat, hdat))
+    i = 0
+    ani = animation.FuncAnimation(fig, animate2,fargs=(xdat2, hdat))
     plt.show()
 
 if __name__ == '__main__':
@@ -67,15 +86,20 @@ if __name__ == '__main__':
     etiqueta = tkinter.Label(window, text='Sistema domotico')
     etiqueta.pack()
     # boton para la temperatura 
-    bototon1 = tkinter.Button(window, text = 'temperatura', command = temperatura)
+    bototon1 = tkinter.Button(window, text = 'Temperatura', command = temperaturaPrnt)
+    bototon1.pack(side= tkinter.RIGHT)
+    bototon1 = tkinter.Button(window, text = 'Humedad', command = humedadPrnt)
     bototon1.pack(side= tkinter.RIGHT)
 
     bototon2 = tkinter.Button(window, text = 'EXIT', command = exit)
     bototon2.pack(side= tkinter.LEFT)
 
-    bototon3 = tkinter.Button(window, text = 'grafica', command = grafica2)
+    bototon3 = tkinter.Button(window, text = 'Grafica Temperatura', command = grafica)
     bototon3.pack(side= tkinter.BOTTOM)
-
+    
+    bototon3 = tkinter.Button(window, text = 'Grafica Humedad', command = grafica2)
+    bototon3.pack(side= tkinter.BOTTOM)
+    
     window.title("SIS domotico")
 
     window.mainloop()
